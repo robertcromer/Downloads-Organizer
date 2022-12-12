3305bb95511d5d35e0ef3ded8beebec35d5b09ca
KeyboardBasher <robert.cromer@dasvoidstudios.tech>
2022-12-11 21:30:38 -0700

    Create main.py
    Initial commit of main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Downloads organizer
+
+__author__ = "KeyboardBasher"
+__credits__ = ["KeyboardBasher"]
+__version__ = "0.0.1"
+__maintainer__ = "KeyboardBasher"
+__status__ = "Development"
+__name__ = "Downloads Organizer"
